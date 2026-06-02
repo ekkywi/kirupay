c9fa593cf2f3d3fbce9dd7b542904b5113961572
Yon Ekky Wijayanto <yonekkywijayanto@outlook.com>
2026-06-02 13:08:26 +0700

    fix(prerender): vercel prerender failure for authenticated routes
    - fix vercel prerender failure for authenticated routes

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -4,6 +4,8 @@ import { getCurrentActor } from "@/lib/auth-service";
 import prisma from "@/lib/neon";
 import { redirect } from "next/navigation";
 
+export const dynamic = "force-dynamic";
+
 export default async function DashboardLayout({ 
   children 
 }: { 
